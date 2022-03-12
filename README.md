9f18d490353e7a95ea775f6111ee66ce0eecce1b
Digital Birth <quotes@digitalbirth.com.au>
2022-03-13 10:25:49 +1100

    update example and readme

--- README.md
+++ README.md
@@ -1,22 +1,45 @@
-BTS7960 Linear Actuator Driver for potentialometer feedback linear actuator
+<div id="top"></div>
+
+# BTS7960 Linear Actuator Driver for potentiometer feedback linear actuator
+
 ============
 
-# How to use
+Designed and tested using STM32 Black Pill
+Built for Arduino
 
-## Define object
+## How to use
+
+### Define object
 `BTS7960AD actuator(R_EN, R_PWM, R_IS, L_EN, L_PWM, L_IS, sensor, strokeLength, debug);`
 
-## Methods in set up
-`actuator.begin();` sets up actuator pins defined in object
+### Pin out for BTS7960
+1 RPWM Forward Level or PWM signal, Active High
+2 LPWM Reverse Level or PWM signal, Active High
+3 R_EN Forward Drive Enable Input, Active High/ Low Disable
+4 L_EN Reverse Drive Enable Input, Active High/Low Disable
+5 R_IS Forward Drive, Side current alarm output
+6 L_IS Reverse Drive, Side current alarm output
+7 Vcc +5V Power Supply micro-controller
+8 Gnd Ground Power Supply micro-controller
 
-`actuator.enable(true);`  enable driver pins R_EN and L_EN bringing them high
+### Methods in set up
+`actuator.init();`  resets actuator id's in EEPROM.
 
-`actuator.calibrate();`   fully extends and retracts to record min max potetiometer values
+`actuator.begin();` sets up actuator pins defined in object and EEPROM memory positions.
+
+`actuator.enable(true);`  enable driver pins R_EN and L_EN bringing them high. (May have issues - connected EN pins to the +ve rail so always on)
+
+`actuator.calibrate(127);`   fully extends and retracts to record min max potentiometer values.
+
+On initial start the calibrate function will test to see if there are values saved in EEPROM, if not it will fully extend and retract and record the min max values of the potentiometer and save to EEPROM, when the board is re-powered, the values can be pulled from EEPROM saving loading time.
+
+The actuators can be recalibrated by calling 
+`actuator.recalibrate(127);`   this is called from a button press in the example sketch.
 
 
-## Methods in loop
+### Methods in loop
 
-### Direction
+#### Direction
 extend  = 1
 
 stop    = 0
@@ -24,18 +47,35 @@ stop    = 0
 retract = -1
 
 
-### Speed
+#### Speed
 input value between 0-255
 
-### Methods
+#### Methods
 `actuator.controlActuator(1, 127);` direction and speed
 
 `actuator.actuateByDistance(75, 1, 50);` distance mm, direction and speed
 
+<!-- ROADMAP -->
+## Roadmap
+
+- [ ] Add Calibration for multiple actuators - Test
+- [ ] Synchronized motor movement function
+- [ ] Add position memory
+    - [ ] Digital read buttons
+
+See the [open issues](https://github.com/digitalbirth/BTS7960AD/issues) for a full list of proposed features (and known issues).
+
+<p align="right">(<a href="#top">back to top</a>)</p>
 
 
+<!-- CONTACT -->
+## Contact
 
+Dean Bateman - [https://www.linkedin.com/in/deanbateman/](https://www.linkedin.com/in/deanbateman/) - quotes@digitalbirth.com.au
 
+Project Link: [https://github.com/digitalbirth/BTS7960AD](https://github.com/digitalbirth/BTS7960AD)
+
+<p align="right">(<a href="#top">back to top</a>)</p>
 
 
 Coded by
--- examples/BTS7960AD_basic_control/BTS7960AD_basic_control.ino
+++ examples/BTS7960AD_basic_control/BTS7960AD_basic_control.ino
@@ -15,45 +15,61 @@
 
 #include <BTS7960AD.h>
 
-#define R_PWM 3                         // define pin 3 for R_PWM pin (output)
-#define R_EN 4                          // define pin 2 for R_EN pin (input)
-#define R_IS 5                          // define pin 5 for R_IS pin (output)
+#define R_PWM PB9                       // define pin 3 for R_PWM pin (output)
+#define R_EN PB7                        // define pin 2 for R_EN pin (input)
+#define R_IS PB5                        // define pin 5 for R_IS pin (output)
 
-#define L_PWM 6                         // define pin 6 for L_PWM pin (output)
-#define L_EN 7                          // define pin 7 for L_EN pin (input)
-#define L_IS 8                          // define pin 8 for L_IS pin (output)
+#define L_PWM PB8                       // define pin 6 for L_PWM pin (output)
+#define L_EN PB6                        // define pin 7 for L_EN pin (input)
+#define L_IS PB4                        // define pin 8 for L_IS pin (output)
 
-#define sensor 8                        // define pin 8 for L_IS pin (output)
-#define strokeLength 150                // define stroke length in mm
+#define sensor PA0                      // define pin 8 for L_IS pin (output)
+#define strokeLength 50                 // define stroke length in mm
 
 #define debug 1                         //change to 0 to hide serial monitor debugging information or set to 1 to view
 
+const int buttonPin = PA1;
+int buttonState = 0;
 
 BTS7960AD actuator(R_EN, R_PWM, R_IS, L_EN, L_PWM, L_IS, sensor, strokeLength, debug);
 
 void setup() {
     Serial.begin(115200);               // setup Serial Monitor to display information
+    actuator.init();                    // reset actuator EEPROM id's
     actuator.begin();                   // set up actuator pins
-    actuator.enable(true);                  // enable driver pins
-    actuator.calibrate();               // set min / max actuator values
+    actuator.enable(true);              // enable driver pins
+    actuator.calibrate(127);            // set min / max actuator values
+    pinMode(buttonPin, INPUT_PULLUP);   // set btn as input and pull up
 }
 
 void loop() {
-    Serial.println("Extending...");
-    actuator.controlActuator(1, 127);
-    delay(1000);
 
-    Serial.println("Retracting...");
-    actuator.controlActuator(-1, 127);
-    delay(1000); // stop for 1 seconds  
+    buttonState = digitalRead(buttonPin);
 
-    Serial.println("Extending 75mm slowly");
-    actuator.actuateByDistance(75, 1, 50);
-    delay(5000); // stop for 5 seconds
-
-    Serial.println("Retracting 25mm full speed");
-    actuator.actuateByDistance(25, -1, 255);
-    delay(5000); // stop for 5 seconds
-
-           
+    // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+    if (buttonState == LOW) {
+        actuator.recalibrate(127);
+    }
+    
+    test();
+      
 }// loop ends
+
+
+void test(){
+  Serial.println("Extending...");
+  actuator.controlActuator(1, 127);
+  delay(1000);
+
+  Serial.println("Retracting...");
+  actuator.controlActuator(-1, 127);
+  delay(1000); // stop for 1 seconds  
+
+  Serial.println("Extending 50mm full speed");
+  actuator.actuateByDistance(strokeLength, 1, 255);
+  delay(5000); // stop for 5 seconds
+
+  Serial.println("Retracting 25mm slowly");
+  actuator.actuateByDistance(strokeLength/2, -1, 50);
+  delay(5000); // stop for 5 seconds
+}
